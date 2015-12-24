89ee5a4edaf39f2089587577ad946cbdf29ea219
James Traver <jtraverjr@gmail.com>
2015-12-24 12:55:22 -0500

    intro to C#

--- 3_inheritance/Doctor.cs
+++ 3_inheritance/Doctor.cs
@@ -1,7 +1,9 @@
-using System; // require('System')
-
 // Doctor inherits from Person
 public class Doctor : Person {
+  public Doctor(int age, string name) {
+    this.Name = name;
+    this.Age = age;
+  }
   public int UpdateAge(int age) {
     int newAge = age * 10000;
     this.Age = newAge;
